dbe24551acb2d7d3c63bb38b54ffe8b2ddd66101
andrew <indecheandrew@gmail.com>
2022-07-27 16:21:27 +0300

    add module export to make available specified codes to another file

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,7 @@
 //The path function initializes the path module that will allow finding and interaction of file paths.
 const path = require('path');
+
+//The module.exports instruct node.js to export specified codes from a given file so that it is accesed by another file.
+module.exports = {
+
+}
