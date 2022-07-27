e2d04f10cb272b5044489fda6879011d6a1e8a1e
andrew <indecheandrew@gmail.com>
2022-07-27 16:47:09 +0300

    add entry to transpile code onto output/dist

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@
 const path = require('path');
 
 //The module.exports instruct node.js to export specified codes from a given file so that it is accesed by another file.
-module.exports = {
+//The entry refers to the starting point of webpack transpiling/bundling process.
+
+module.exports = {  //webpack will take source code from ./src folder and transpile it into the dist folder
+    entry: {
+    app:'./src/index.js' 
+},
+    output:{
+    path: path.resolve(__dirname,'dist')
+    },
 
 }
