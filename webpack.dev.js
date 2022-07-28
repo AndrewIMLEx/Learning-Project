13914b9ad6e4cd18df97e51991181944757bea2a
AndrewIndeche <indecheandrew@gmail.com>
2022-07-28 21:31:01 +0300

    initialize path module to allow interaction between files

---
+++ webpack.dev.js
@@ -0,0 +1,7 @@
+//configuration for webpack development server
+
+//The path function initializes the path module that will allow finding and interaction of file paths.
+const path = require("path");
+
+//initialize webpack-merge module for merging production and development configurations.
+const {merge} = require("webpack-merge"); 
