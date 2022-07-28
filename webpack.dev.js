9252a89dfc4322c039d1125e1fb1062fc8d284bd
AndrewIndeche <indecheandrew@gmail.com>
2022-07-28 21:45:23 +0300

    initialize common webpack configuration settings for use by other modules

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 //configuration for webpack development server
-
 //The path function initializes the path module that will allow finding and interaction of file paths.
 const path = require("path");
-
 //initialize webpack-merge module for merging production and development configurations.
 const {merge} = require("webpack-merge"); 
+//iitialize common webpack settings module for use by production and development settings
+const common = require("./webpack.config.js")
