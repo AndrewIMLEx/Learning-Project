cc40d448099d0cc70047483d0b3e01ada68d3564
AndrewIndeche <indecheandrew@gmail.com>
2022-07-30 10:31:34 +0300

    initialize webpack-merge module for merging production and development configurations

---
+++ webpack.prod.js
@@ -0,0 +1,3 @@
+//initialize webpack-merge module for merging production and development configurations.
+const { merge } = require('webpack-merge'); 
+//initialize common/main configuration for use by production and development configuration.
