354d24b3d6f0cda4907ef92f496abb313f0974cd
AndrewIndeche <indecheandrew@gmail.com>
2022-07-30 10:37:02 +0300

    set webpack mode to production mode and export module for use by other modules

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,9 @@
 //initialize webpack-merge module for merging production and development configurations.
 const { merge } = require('webpack-merge'); 
 //initialize common/main configuration for use by production and development configuration.
+const common = require("./webpack.config.js");
+
+//set webpack mode to production mode and export module for use by other modules
+module.exports = merge(common,{
+    mode:'production'
+})
