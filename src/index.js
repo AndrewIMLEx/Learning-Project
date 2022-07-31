688847ebd494a4f91b1ff1b13ec8bed79e3ac954
AndrewIndeche <indecheandrew@gmail.com>
2022-07-31 21:34:31 +0300

    configure react strict mode to carry out checks on child components

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import React from 'react' //imports react library(JSX) codes from 'react' folder
 // assigns the code into a'React' variable.
 import ReactDOM from "react-dom"; // imports DOM specific methods codes from 'react-dom' folder in 'node-modules'
 import App from "./App.js"; //imports the code from the App component
+
+ReactDOM.render( //React.StrictMode checks for problems in it's descendant components (App)
+    <React.StrictMode> 
+    <App />
+  </React.StrictMode>,
+);
