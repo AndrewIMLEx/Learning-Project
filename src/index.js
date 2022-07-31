63ff3cbd4bef9aef9688d567040941b310a52ae5
AndrewIndeche <indecheandrew@gmail.com>
2022-07-31 21:16:43 +0300

    import modules to use react features

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 //index is a naming convention that lets a user know that they are the main files.
 //The index.js component mounts the main react component onto the "root element
-import React from 'react' //imports the React library (JSX) for use in the project
+import React from 'react' //imports react library(JSX) codes from 'react' folder in node-modules and
+// assigns the code into a'React' variable.
+import ReactDOM from "react-dom"; // imports DOM specific methods codes from 'react-dom' folder in 'node-modules'
+import App from "./App.js"; //imports the code from the App component
