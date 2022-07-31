2a6e0b0d7134ee998f0cee6df5cbf6ffc2a2e8a8
AndrewIndeche <indecheandrew@gmail.com>
2022-07-31 20:41:15 +0300

    import main react library

---
+++ src/index.js
@@ -0,0 +1,3 @@
+//index is a naming convention that lets a user know that they are the main files.
+//The index.js component mounts the main react component onto the "root element
+import React from 'react' //imports the React library (JSX) for use in the project
