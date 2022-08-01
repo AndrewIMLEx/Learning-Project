68c6969aec1562cbfcd7bf11aac44724ab62e030
andrew <indecheandrew@gmail.com>
2022-08-01 21:08:00 +0300

    import code from common webpack to minify code in webpack development

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,5 @@
 //configuration for webpack development server
 //The path function initializes the path module that will allow finding and interaction of file paths.
-const { dirname } = require("path");
-const path = require("path");
 //initialize webpack-merge module for merging production and development configurations.
 const {merge} = require("webpack-merge"); 
 //initialize common webpack settings module for use by production and development settings.
@@ -9,30 +7,13 @@ const common = require("./webpack.config.js");
 //export module with merged/bundled common and development settings.
 module.exports = merge(common,{
     mode:"development",//set webpack configuration to development mode
-    entry:[
-        'react-hot-loader/patch',
-        './src/index.js' //Add index.js as the entry point of bundling
-    ],
     devtool: 'inline-source-map',
     devServer:{
         'static':{
             directory:'./dist',
 
         },
-    },
-    output:{
-      filename:'bundle.js',
-      path:path.resolve(__dirname,'dist'),
-    },
-    module:{
-        rules:[
-        {
-        test:/.(js|jsx)$/, //add webpack rule to configure js and jsx
-        use:'babel-loader',//configure babel to convert/transpile JSX code into a browser readable js format
-        exclude:/node_modules/  //rule to exclude node_modules when bundling
-        }
-        ]
-     },  
+    },  
 });
 
 
