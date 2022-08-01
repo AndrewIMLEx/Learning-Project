7c8ed9ae4be898958f89110c76a87f90770d357b
andrew <indecheandrew@gmail.com>
2022-08-01 12:31:55 +0300

    add missing semicolons to correct errors raised by CI/CD tools

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import React from 'react' //imports react library(JSX) codes from 'react' folder in node-modules and
+import React from 'react'; //imports react library(JSX) codes from 'react' folder in node-modules and
 // assigns the code into a'React' variable.
 
 const App = () => {//A JSX function made of Javascript and XML code using arrow function that
 //returns the word 'Hello world'
     return <div>
     Hello World!
-    </div>
+    </div>;
     
     };
 export default App; //Used to export the App function to be used by other components
