4e4be2dbe8268b7ccbc365209db74a5988c49aa1
andrew <indecheandrew@gmail.com>
2022-08-02 14:14:53 +0300

    remove react hot loader due to clash in dependencies

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from 'react'; //imports react library(JSX) codes from 'react' folder in node-modules and
 // assigns the code into a'React' variable.
-
 const App = () => {//A JSX function made of Javascript and XML code using arrow function that
 //returns the word 'Hello world'
     return <div>
