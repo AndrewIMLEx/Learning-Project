c30cb40234557fdb812cf6e062c8088ad842b2cf
AndrewIndeche <indecheandrew@gmail.com>
2022-08-07 16:07:27 +0300

    remove trailing comma to fix language rulee

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import React from 'react'; //imports react library(JSX) codes from 'react' folder in node-modules and
 // assigns the code into a'React' variable.
 import ReactDOM from "react-dom"; // imports DOM specific methods codes from 'react-dom' folder in 'node-modules'
-import App from "./app.js"; //imports the code from the App component
+import App from "./App.js"; //imports the code from the App component
 
 ReactDOM.render( //React.StrictMode checks for problems in it's descendant components (App)
     <React.StrictMode> 
