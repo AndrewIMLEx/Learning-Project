44c54309cbb1306760a08013ccfd43359892de89
AndrewIndeche <indecheandrew@gmail.com>
2022-08-08 17:14:48 +0300

    remove hot loader text to avoid duplication with common config file

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,3 @@
-
 //initialize webpack-merge module for merging production and development configurations.
 const { merge } = require('webpack-merge'); 
 //initialize common/main configuration for use by production and development configuration.
@@ -6,13 +5,6 @@ const common = require("./webpack.config.js");
 //set webpack mode to production mode and export module for use by other modules
 module.exports = merge(common,{
     mode:'production',
-    module:{
-        rules:[
-        {
-        exclude:/node_modules/,  //rule to exclude node_modules when bundling
-        }
-       ]
-   },
  }
 );
 
