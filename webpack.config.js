6a194f5e81fc3914730a0393c41b1ce4703efd0e
AndrewIndeche <indecheandrew@gmail.com>
2022-08-08 17:16:05 +0300

    configure css and its loader to work with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,8 @@ filename: 'bundle.js', //script to bundle the contents of index.js to bundle.js
 module:{
 rules:[
 {
-test:/.(js|jsx)$/, //add webpack rule to configure js and jsx
+    test: /\.css$/i,
+    use: ["css-loader"], //configure css and its loader to work on webpack
 use:'babel-loader',//configure babel to convert/transpile JSX code into a browser readable js format
 exclude: /node_modules/  //rule to exclude node_modules when bundling
 }
