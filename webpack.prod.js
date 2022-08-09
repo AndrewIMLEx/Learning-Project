acd25c3cc3202ee8015a3b3d1a0a18d7248d42f9
AndrewIndeche <indecheandrew@gmail.com>
2022-08-09 23:45:57 +0300

    change imports to const calls to match es6 rules

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,7 @@
-
 //initialize webpack-merge module for merging production and development configurations.
-import { merge } from 'webpack-merge'; 
+const { merge } = require('webpack-merge'); 
 //initialize common/main configuration for use by production and development configuration.
-import common from "./webpack.config.js";
+const common = require("./webpack.config.js");
 //set webpack mode to production mode and export module for use by other modules
 export default merge(common,{
     mode:'production',
