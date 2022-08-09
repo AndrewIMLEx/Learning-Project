ce95a40f43062a1392369b34c0fe96f47493a9e6
AndrewIndeche <indecheandrew@gmail.com>
2022-08-09 23:00:41 +0300

    edit hello world text to test hot loader

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { hot } from 'react-hot-loader/root';
 const App = () => {//A JSX function made of Javascript and XML code using arrow function that
 //returns the word 'Hello world'
     return <div>
-    Hello World Its Me Andrew
+    Hello World
     </div>;
     };
 export default hot(App); //Used to export the App function to be used by other components
