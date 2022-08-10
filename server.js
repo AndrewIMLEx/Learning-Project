2314ed0af0ba36587620f4e4ea32eb5f69e6445c
AndrewIndeche <indecheandrew@gmail.com>
2022-08-10 21:58:38 +0300

    configure port to communicate between servers

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express(); //Calls express method as an object
 
 app.use(express.static(path.join(__dirname, '/dist'))); //the express app looks up for files relative to the static directory 'dist'
 //path.join returns a normalized path by merging two paths together
-app.set('port', process.env.PORT || 8080); //the app assigns a port number where data endpoints will be directed to a particular service
+app.set('port', process.env.PORT || 6000); //the app assigns a port number where data endpoints will be directed to a particular service
 var server = app.listen(app.get('port'), function() { //the app.listen listens to connections on the specified port and produces an output
 //of listening to port
     console.log('listening on port ', server.address().port);
