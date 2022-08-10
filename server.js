4c78078447f49d078441be3286e09eab1fbf62a6
andrew <indecheandrew@gmail.com>
2022-08-10 13:42:31 +0300

    call express method as an object to create a new express app

---
+++ server.js
@@ -0,0 +1,4 @@
+var path = require('path'); //Node.js module that is used to transform paths
+var express = require('express'); //Module that returns a function reference for express
+
+var app = express(); //Calls express method as an object
