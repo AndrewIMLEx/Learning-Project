1e189fefc42bb907664b75ecf170eb1edfdeb17e
andrew <indecheandrew@gmail.com>
2022-08-10 15:20:52 +0300

    assign port number for data  endpoints to be directed to

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ var path = require('path'); //Node.js module that is used to transform paths
 var express = require('express'); //Module that returns a function reference for express
 
 var app = express(); //Calls express method as an object
+
+app.use(express.static(path.join(__dirname, 'dist'))); //the express app looks up for files relative to the static directory 'dist'
+//path.join returns a normalized path by merging two paths together
+app.set('port', process.env.PORT || 8080); //the app assigns a port number where data endpoints will be directed to a particular service
