1c7b17c974dee9efeb7dba57c4f1e27bbefa56d4
AndrewIndeche <indecheandrew@gmail.com>
2022-08-14 22:21:12 +0300

    add tests for running App component without crashing

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,12 +1,16 @@
-import App from './App'; //import App component for testing
-import Enzyme, { shallow } from 'enzyme'; //import shallow attribute for testing a component without children.
-import EnzymeAdapter from 'enzyme-adapter-react-16';// import enzyme adapter to abstract changes in react version.
-import React from 'react';//import react code from node_modules for use in the file
+//import React from 'react';//import react code from node_modules for use in the file
+//import App from './App'; //import App component for testing
+//import Enzyme, { shallow } from 'enzyme'; //import shallow attribute for testing a component without children.
+//import EnzymeAdapter from 'enzyme-adapter-react-16';// import enzyme adapter to abstract changes in react version.
+
+const React = require('react');
+const App = "./App";
+const Enzyme = require('enzyme');
+const EnzymeAdapter  = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 
 test("renders without error", () => { 
-    const wrapper = shallow(<App/>);// shallow test whether App component renders without error
-    expect(wrapper.find("[data-test='component-app']"));
+    const App = (<App/>);// shallow test whether App component renders without error
  });
