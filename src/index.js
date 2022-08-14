a00d66b85f7b37bd8221b6c48eae18a5c7ca9480
AndrewIndeche <indecheandrew@gmail.com>
2022-08-14 16:31:53 +0300

    correct import for App component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import React from 'react'; //imports react library(JSX) codes from 'react' folder in node-modules and
 // assigns the code into a'React' variable.
 import ReactDOM from "react-dom"; // imports DOM specific methods codes from 'react-dom' folder in 'node-modules'
-import App from "./App.js"; //imports the code from the App component
+import App from "./App"; //imports the code from the App component
 
 var appNode = document.getElementById("learning"); //return element with  specified value
 ReactDOM.render(<App/>, appNode);
