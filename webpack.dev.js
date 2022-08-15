710161fc9c3f6dbd32ffd6ba7fa14ab4e993bbd8
AndrewIndeche <indecheandrew@gmail.com>
2022-08-15 12:40:53 +0300

    add react-hot-loader patch to hotload in dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,26 +1,27 @@
 //configuration for webpack development server
 //The path function initializes the path module that will allow finding and interaction of file paths.
+const { dirname } = require("path");
+const path = require("path");
 //initialize webpack-merge module for merging production and development configurations.
 const {merge} = require("webpack-merge"); 
 //initialize common webpack settings module for use by production and development settings.
 const common = require("./webpack.config.js");
 //export module with merged/bundled common and development settings.
 module.exports = merge(common,{
-    devtool: "inline-source-map",
     mode:"development",//set webpack configuration to development mode
+    entry:[ 
+        'react-hot-loader/patch',
+        './src/index.js' //Add index.js as the entry point of bundling
+    ],
+    devtool: 'inline-source-map',
     devServer:{
-        'static':{
-            hot:true
+        static:{
+            directory:path.join(dirname,"./dist")
         },
-    }, 
-    module:{
-        rules:[
-        {
-        test:/.(js|jsx)$/, //add webpack rule to configure js and jsx
-        use:'babel-loader',//configure babel to convert/transpile JSX code into a browser readable js format
-        exclude: /node_modules/  //rule to exclude node_modules when bundling
-        }
-        ]
-        }, 
+    },
+    output:{
+      filename:'bundle.js',
+      path:path.resolve(__dirname,'dist'),
+      clean:true,
+    }
 });
-
