44d0ca9e525b682b11d9f93f30c995835c19ae22
AndrewIndeche <indecheandrew@gmail.com>
2022-08-15 16:15:05 +0300

    add imports and function to shallow test App component

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,12 +1,14 @@
-const React = 'react';//import App component for testing
-const Enzyme = require('enzyme',{ shallow } );//import shallow attribute for testing a component without children.
+var React = require('react');//import App component for testing
+const Enzyme = require('enzyme');//import shallow attribute for testing a component without children.
 const EnzymeAdapter = require('enzyme-adapter-react-16');// import enzyme adapter to abstract changes in react version.
-const  App = require('./App');//import react code from node_modules for use in the file
+const  App = './App';//import react code from node_modules for use in the file
+const { shallow } =  require('enzyme');
+
+
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-
-test("renders without error", () => { 
-    const wrapper = shallow(<App/>);// shallow test whether App component renders without error
-    expect(wrapper.find("[data-test='component-app']"));
- });
+it('renders without crashing', () => {
+    shallow(<App />);
+   });
+  
