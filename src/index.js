5d73b6ed6e2f4b547d8a9f6225a3701a97d8f389
andrew <andrew.indeche@mawazoinstitute.org>
2022-08-24 13:35:52 +0300

    remove non-initial code to have a clean working branch

--- src/index.js
+++
@@ -1,9 +0,0 @@
-//index is a naming convention that lets a user know that they are the main files.
-//The index.js component mounts the main react component onto the "root element
-import React from 'react'; //imports react library(JSX) codes from 'react' folder in node-modules and
-// assigns the code into a'React' variable.
-import ReactDOM from "react-dom"; // imports DOM specific methods codes from 'react-dom' folder in 'node-modules'
-import App from "./App.js"; //imports the code from the App component
-
-var appNode = document.getElementById("learning"); //return element with  specified value
-ReactDOM.render(<App/>, appNode);
