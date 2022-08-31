e5c2bead19762cfc445166625ff06950b7fec674
andrew <andrew.indeche@mawazoinstitute.org>
2022-08-31 10:33:12 +0300

    add PORT to configure PORT for env

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 //configuration for webpack development server
 //The path function initializes the path module that will allow finding and interaction of file paths.
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 //initialize webpack-merge module for merging production and development configurations.
 const {merge} = require("webpack-merge"); 
@@ -10,8 +11,10 @@ dotenv.config();
 //initialize common webpack settings module for use by production and development settings.
 const common = require("./webpack.config.js");
 //export module with merged/bundled common and development settings.
+
+//set webpack configuration to development mode
 module.exports = merge(common, {
-    mode: "development",//set webpack configuration to development mode
+    mode: "development",
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
