b246ac61f595c50a56be7b87601d8e03f8ae5b29
andrew <andrew.indeche@mawazoinstitute.org>
2022-09-21 17:05:53 +0300

    correct alignment

--- __Tests__/app.test.js
+++ __Tests__/app.test.js
@@ -1,11 +1,8 @@
-import TestRenderer from 'react-test-renderer';
 import React from 'react';
-
-import App from './App';
+import TestRenderer from 'react-test-renderer';
+import App from './App/App.jsx';
 
 it('renders a correctly', () => {
-  const tree = TestRenderer.create(<App/>).toJSON();
-  expect(tree).toMatchSnapshot();
+  const tree = TestRenderer.create(<App />).toJSON();
+expect(tree).toMatchSnapshot();
 });
-
-
