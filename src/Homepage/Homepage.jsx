32984a88b451df70265f4849bab8de8aff1cd9e6
andrew <andrew.indeche@mawazoinstitute.org>
2022-10-06 10:36:01 +0300

    change Homepage to jsx

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -2,14 +2,7 @@ import React from "react";
 
 class Homepage extends React.Component{
     render() {
-      return (
-      <>
-      <div className="card">
-        <h1>Guess The Number</h1> 
-        <h2>Non Logged In Gameplay</h2>
-        </div>
-      </>
-      )
+      return (<></>)
     }
 }
 
