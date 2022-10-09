a8586fc115a535bc1aa6a6ce18632f4708b3c2b6
andrew <andrew.indeche@mawazoinstitute.org>
2022-10-09 16:12:14 +0300

    align header2 center

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -6,6 +6,7 @@ class Homepage extends React.Component{
       <>
       <div className="card">
         <h1>Guess The Number</h1> 
+        <h2>Registration Page</h2>
         </div>
       </>
       )
