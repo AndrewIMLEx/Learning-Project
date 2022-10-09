f19aed8bab0e534ba22b33364a3280e854b414d7
andrew <andrew.indeche@mawazoinstitute.org>
2022-10-09 22:02:28 +0300

    add header 2

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -6,7 +6,7 @@ class Homepage extends React.Component{
       <>
       <div className="card">
         <h1>Guess The Number</h1> 
-        <h2>Registration Page</h2>
+        <h2>Non Logged In Gameplay</h2>
         </div>
       </>
       )
