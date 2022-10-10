909acd9ee4e958a89cdbdedb658d2ec6f6ad056c
andrew <andrew.indeche@mawazoinstitute.org>
2022-10-10 23:27:00 +0300

    test Rules component

---
+++ __Tests__/rules.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Rules from '../src/Homepage/Rules';
+import renderer from 'react-test-renderer';
+
+describe("Rules page",() =>{
+    it('renders the rules page', () => {
+      const tree = renderer.create(<Rules />).toJSON();
+      expect(tree).toMatchSnapshot();
+    })
+  })
