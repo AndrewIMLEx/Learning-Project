c58e81c57a61d9fcacbc3fab72b7c963001420b7
andrew <andrew.indeche@mawazoinstitute.org>
2022-10-10 13:16:43 +0300

    add Rules component

---
+++ src/Homepage/Rules.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Rules extends React.Component{
+    render() {
+      return (
+      <>
+      <div className="rules">
+        <h1>How to Play</h1>
+        </div>
+      </>
+      )
+    }
+}
+
+export default Rules
