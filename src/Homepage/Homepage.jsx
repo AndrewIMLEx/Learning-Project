43340fbf9a135ced81a726b89b33bc87d43b63d4
andrew <andrew.indeche@mawazoinstitute.org>
2022-10-10 13:38:53 +0300

    render Rules component

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Rules from "./Rules";
 class Homepage extends React.Component{
     render() {
       return (
@@ -7,6 +7,7 @@ class Homepage extends React.Component{
       <div className="card">
         <h1>Guess The Number</h1> 
         <h2>Non Logged In Gameplay</h2>
+        <Rules/>
         </div>
       </>
       )
