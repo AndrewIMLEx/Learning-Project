c5a226b541b1afe4c1c93a1d5ac244b9f44cf1e5
andrew <andrew.indeche@mawazoinstitute.org>
2022-11-07 14:28:44 +0300

    add div to Levels component

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Rules from "./Rules";
+import Levels from "./Levels";
+
 class Homepage extends React.Component{
     render() {
       return (
@@ -8,6 +10,9 @@ class Homepage extends React.Component{
         <div className = "header">
         <h1>Guess The Number</h1> 
         <h2>Non Logged In Gameplay</h2>
+        </div>
+        <div className = "levels">
+        <Levels/>
         </div>
         <Rules/>
         </div>
