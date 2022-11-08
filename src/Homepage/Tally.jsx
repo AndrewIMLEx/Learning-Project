4b70db80748d5adba589d5a95ed9a902e040677f
andrew <andrew.indeche@mawazoinstitute.org>
2022-11-08 16:25:50 +0300

    add Tally form

--- src/Homepage/Tally.jsx
+++ src/Homepage/Tally.jsx
@@ -4,6 +4,20 @@ class Tally extends React.Component{
     render(){
         return(
             <>
+            <h4>Game Tally</h4>
+            <form id = "LevelForm">
+            <label for="won">Games Won</label>
+            <input/>
+             </form>
+             <br/>
+             <form id = "LevelForm">
+             <label for="won">Games Lost</label>
+            <input/>
+             </form>
+             <form id = "LevelForm">
+             <label for="gamesplayed">Total games Played</label>
+            <input/>
+             </form>
             </>
         )
     }
