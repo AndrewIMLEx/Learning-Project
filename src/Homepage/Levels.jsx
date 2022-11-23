e2114c8b3096984c1a8b3a2c7116642583218d18
AndrewIMLEx <indecheandrew@outlook.com>
2022-11-23 12:54:24 +0300

    add function to change difficulty

--- src/Homepage/Levels.jsx
+++ src/Homepage/Levels.jsx
@@ -4,24 +4,32 @@ class Levels extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            difficult: false,
-            standard: false,
-            optionSelected: '',
+            levelSelected: 'Make a Selection',
+            ButtonClick: '',
             standardNum : Math.floor(Math.random() * 10) + 1,
             difficultNum : Math.floor(Math.random() * 100) + 1
-        }
+        };
+        this.standard = this.standard.bind(this);
+        this.difficult = this.difficult.bind(this);
     }
+    standard(){
+        this.setState({levelSelected:'Standard'});
+    }
+    difficult(){
+        this.setState({levelSelected:'Difficult'});
+    }
+
 render(){
     return(
         <>
         <div className="levelselect">
         <h4>Select Difficulty Level</h4>
-        <button id="standard" type  = "submit">Standard</button>
-        <button id="difficult" type = "submit">Difficult</button>
+        <button id="standard" onClick ={this.standard}>Standard</button>
+        <button id="difficult" onClick ={this.difficult}>Difficult</button>
         </div>
         <div className="levelCard">
         <h3 style={{color: "black"}}>Selected Level</h3>
-        <p style={{color: "red"}}>STANDARD</p>
+        <p style={{color: "red"}}>{ this.state.levelSelected}</p>
         <form id = "LevelForm">
             <input id = "Levelsinput"/>
         </form>
